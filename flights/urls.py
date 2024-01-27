eec0cce5841d3a64dcc1e07a8e95a122d9e09335
Manoj Mohan <haimanojkumar@gmail.com>
2024-01-27 10:41:43 +0530

    add login/logout functionality

--- flights/urls.py
+++ flights/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('<int:flight_id>', views.flight, name="flight"),
     path('<int:flight_id>/book', views.book, name="book"),
+    path('login', views.login_view, name="login"),
+    path('logout', views.logout_view, name="logout"),
 ]
--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -5,8 +6,32 @@ from .models import Flight, Passenger
 
 
 def index(request):
+    print("status of user auth: ", request.user.is_authenticated)
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
     return render(request, "flights/index.html",
                   {"flights": Flight.objects.all()})
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username,
+                            password=password)
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "flights/login.html",
+                          {"message": "Invalid Credentials"})
+
+    return render(request, "flights/login.html")
+
+def logout_view(request):
+    logout(request)
+    return render(request, "flights/login.html",
+                  {"message": "Logged out"})
 
 def flight(request, flight_id):
     flight = Flight.objects.get(id=flight_id)
@@ -24,3 +49,5 @@ def book(request, flight_id):
         passenger = Passenger.objects.get(pk=passenger_id)
         passenger.flights.add(flight)
         return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
+
+
